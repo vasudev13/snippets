0f5a10ecf4d5e1a6afc6a1ce5d987dcdabc580e0
Vasudev Awatramani <30664477+vasudev13@users.noreply.github.com>
2021-03-07 18:56:47 +0530

    Update and rename colab.py to snippet.py

--- snippet.py
+++ snippet.py
@@ -2,3 +2,7 @@
 !git clone https://github.com/Atcold/pytorch-Deep-Learning.git
 import os
 os.chdir("/content/pytorch-Deep-Learning")
+
+
+resnet18=torchvision.models.resnet18(pretrained=True)
+resnet18.fc=nn.Linear(in_features=512,out_features=num_class)
